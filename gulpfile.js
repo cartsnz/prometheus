c0f3a388a3242677e87ed7d83a4f10fb6ff924ba
Nick Carter <nick.carter@shopify.com>
2020-04-27 16:03:26 +1200

    Set up of dev environment with gulp and browsersync

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+
+require('dotenv').config();
+
+gulp.task('sass', function() {
+  return gulp.src('./styles/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(rename('theme.css'))
+    .pipe(gulp.dest('./assets/'))
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+        // proxy: "https://kiwi-bikes.myshopify.com?preview_theme_id=80627531798",
+        proxy: process.env['THEME_PREVIEW_URL'],
+        files: '/var/tmp/theme_ready',
+        // Disable pop-over notification in browser for updates.
+        notify: false,
+        // By default browser-sync injects its script after the opening <body> tag.
+        // This appears to conflict with scripts from Shopify. To solve this, the
+        // browser-sync script is now being injected before the closing </body> tag:
+        snippetOptions: {
+          rule: {
+            match: /<\/body>/i,
+            fn: (snippet, match) => {
+              return snippet + match;
+            }
+          }
+        },
+        // The reloadDelay value was determined experimentally, ensuring that the code
+        // changes were processed and deployed by Shopify by the time browser-sync
+        // refreshes the browser.
+        // https://github.com/Shopify/themekit/issues/688
+        reloadDelay: 2000
+  });
+  
+  // After initializing browser-sync, watch for sass changes
+  gulp.watch('./styles/**/*.scss', gulp.series('sass'))
+})
+
+// Run serve task as default gulp task
+gulp.task('default', gulp.series('serve'));
